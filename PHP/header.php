484a4bdc93472aaa44170dbe15591296eca563ea
Silorey <josephsilorey@outlook.com>
2022-12-12 00:34:06 -0500

    Dynamic heading and footer
    Added a dynamic header and foot to index file for the banner.

---
+++ PHP/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Banner Login</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css"
+</head>
+<body>
+
+	<nav>
+		<div class="wrapper">
+			<a href="index.php">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="discover.php">About Us</a></li>
+				<li><a href="blog.php">Find Blogs</a></li>
+				<li><a href="signup.php">Sign Up</a></li>
+				<li><a href="login.php">Log In</a></li>
+			</ul>
